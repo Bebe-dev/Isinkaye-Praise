2e31a92f88ae20a40a8fbc525570b00fb0f0d4ee
bebe-dev <praiseisinkaye@gmail.com>
2026-02-18 21:41:16 +0100

    updates

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,5 +1,5 @@
 import { PROJECTS } from "../assets/details";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   return (
@@ -15,7 +15,12 @@ export default function Projects() {
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div whileInView={{opacity: 1, x: 0}} initial={{opacity: 0, x: -100}} transition={{duration: 1}} className="relative w-full lg:w-1/4">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="relative w-full lg:w-1/4 group"
+            >
               <img
                 src={project.image}
                 alt={project.title}
@@ -23,16 +28,31 @@ export default function Projects() {
                 height={250}
                 className="mb-6 rounded"
               />
-              <div className="absolute inset-0 flex flex-col justify-start items-center w-[250px] gap-2 top-0 bottom-0 left-0 right-0 bg-black/60 p-4">
-                <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-stone-300 hover:text-white text-sm bg-black p-2 rounded-xl ">
+              <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-start items-center w-[250px] gap-2 top-0 bottom-0 left-0 right-0 bg-black/60 p-4">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-stone-300 hover:text-white text-sm bg-black p-2 rounded-xl "
+                >
                   Live Demo
                 </a>
-                <a href={project.repo} target="_blank" rel="noopener noreferrer" className="text-stone-300 hover:text-white text-sm bg-black p-2 rounded-xl ">
+                <a
+                  href={project.repo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-stone-300 hover:text-white text-sm bg-black p-2 rounded-xl "
+                >
                   View Repository
                 </a>
               </div>
             </motion.div>
-            <motion.div whileInView={{opacity: 1, x: 0}} initial={{opacity: 0, x: 100}} transition={{duration: 1}} className="w-full max-w-xl lg:-3/4">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:-3/4"
+            >
               <h3 className="mb-2 font-semibold text-2xl">{project.title}</h3>
               <p className="mb-4 text-stone-400">{project.description}</p>
               {project.technologies.map((tech, techIndex) => (
